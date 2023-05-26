57d4d12e6d6447e6a92ad60f7c05fa206fa87a13
Vladusick <samuelfs@mail.ru>
2023-05-26 15:20:49 +0400

    added basic operations

--- src/main/java/calculator/fullstack/services/CalculatorService.java
+++ src/main/java/calculator/fullstack/services/CalculatorService.java
@@ -6,7 +6,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class CalculatorService {
 
+    //тут потом заинжектить репу надо будет, когда буду делать подключение к базе
+
     public CalculatorDto addition(Integer a, Integer b) {
         return new CalculatorDto(a, b, "addition", a + b);
+    }
+
+    public CalculatorDto subtraction(Integer a, Integer b) {
+        return new CalculatorDto(a, b, "subtraction", a - b);
+    }
+
+    public CalculatorDto multiplication(Integer a, Integer b) {
+        return new CalculatorDto(a, b, "multiplication", a * b);
+    }
+
+    public CalculatorDto division(Integer a, Integer b) {
+        return new CalculatorDto(a, b, "division", a / b);
     }
 }
